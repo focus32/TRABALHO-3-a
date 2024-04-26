88c6fedd5553b9880f56a24d28f233fdf0189f9c
focus32 <106530038+focus32@users.noreply.github.com>
2024-04-26 08:28:21 -0300

    ol

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ for(let i=0;i <botoes.length;i++){
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-30T00:00:00");
+const tempoObjetivo1 = new Date("2024-07-30T00:00:00");
 const tempoObjetivo2 = new Date("2024-06-30T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-10-30T00:00:00");
